691615439deaf0a7a03928cc5cbc67f71a0f6499
OmingoEmma <emmaomingo@gmail.com>
2024-03-26 16:37:00 +0300

    Completed conditional Lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(somevalue){
   // Write your code here!
+  if (somevalue <= 199){
+    return "This one is on me!";
+  }
+  else if(somevalue <=1500){
+    return "That will be twenty bucks.";
+  }
+  else if (somevalue <=2001){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(somevalue <= 2501){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(somevalue){
   // Write your code here!
+  return somevalue === "NYC" ?"Ok, sounds good.": "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(somevalue){
   // Write your code here!
+  switch (somevalue){
+    case("generous"):
+    return "Thank you so much."
+    case("not as generous"):
+    return "Thank you. "
+    case("thanks for everything"):
+      return "Bye. ";
+
+  }
 }
